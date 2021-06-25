734982a13eba4a4f2cf002b97672443ec62f503a
ismail97-hub <ismailbelkadi4@gmail.com>
2021-06-25 21:50:40 +0100

    initial commit

---
+++ lib/Pages/Home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({ Key key }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          FlatButton(
+          color: Colors.blueAccent,
+          onPressed: (){
+
+          }, 
+          child: Text("Page 1",style: TextStyle(fontWeight: FontWeight.bold))),
+          FlatButton(
+          color: Colors.orangeAccent,
+          onPressed: (){
+
+          }, 
+          child: Text("Page 2",style: TextStyle(fontWeight: FontWeight.bold))),
+        ],
+      ),
+    );
+  }
+}
